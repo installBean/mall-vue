d881a5b2eee96084248d17dc0721a30daf34b5a6
installBean <denglizx@qq.com>
2024-02-26 17:55:26 +0800

    test

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -4308,7 +4308,7 @@ export const prefix = (url) => {
   if (url && url.startsWith('http')) {
     return url
   } else {
-    url = `http://backend-api-01.newbee.ltd${url}`
+    url = `http://localhost:8888${url}`
     return url
   }
 }
--- src/main.js
+++ src/main.js
@@ -51,7 +51,7 @@ app.config.globalProperties.$filters = {
     if (url && url.startsWith('http')) {
       return url
     } else {
-      url = `http://backend-api-01.newbee.ltd${url}`
+      url = `http://localhost:8888${url}`
       return url
     }
   }
